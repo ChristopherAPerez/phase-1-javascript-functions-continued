7a7af4d2ce5ff7f8d2bad3cc4eee8a480236e081
Christopher Perez <ChrisAmadeusPerez@gmail.com>
2022-06-09 22:11:58 -0400

    commit tree c4d3fcac0b9187ff623529771b479afddde58881

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(special) {
+    if (special === "*") {
+        return function result(emphatic = "a hard worker") {
+            return `You are ${special}${emphatic}${special}!`;
+        }
+    } else if (special === "||") {
+        return function result(emphatic = 'a dedicated programmer') {
+            return `You are ${special}${emphatic}${special}!`;
+        }
+    }
+}
